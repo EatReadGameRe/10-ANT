61bd4cde97995261ce3c3455a5cbab973b8f420b
EatReadGameRe <73855904+EatReadGameRe@users.noreply.github.com>
2020-11-11 09:29:18 +0100

    Update jspsych.js

--- js/jspsych/jspsych.js
+++ js/jspsych/jspsych.js
@@ -76,7 +76,7 @@ var jsPsych = (function() {
     }    
     
     // add CSS class to DOM_target
-    DOM_target.addClass('jspsych-display-element');
+    $(DOM_target).addClass('jspsych-display-element');
 
     // create experiment timeline
     timeline = new TimelineNode({
